784335734e10277a772e90f2bdbe0e7970b3953b
Julia <sorulai@yandex.ru>
2021-01-19 23:13:15 +0300

    3 задание

--- practice-3/task-3.py
+++ practice-3/task-3.py
@@ -1,2 +1,11 @@
 # 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
 
+
+def my_func(num1, num2, num3):
+    list_num = [num1, num2, num3]
+    list_num.sort(reverse=True)
+    list_num.pop()
+    return sum(list_num)
+
+
+print(my_func(1,10,7))
